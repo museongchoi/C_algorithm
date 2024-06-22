220538072edfff98ba6dc8cd9c5923f26cf48a80
museongchoi <97529960+museongchoi@users.noreply.github.com>
2024-06-22 19:14:06 +0900

    2557

---
+++ Input_Output/B5_2557.cpp
@@ -0,0 +1,28 @@
+// // 1. iostream
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     cout << "Hello World!" << endl;
+// 		//cout << "Hello World!";
+//     return 0;
+// }
+
+// // 2. stdio.h
+// #include <stdio.h>
+
+// int main()
+// {
+//     printf("Hello World!");
+//     return 0;
+// }
+
+// // 3. cstdio
+// #include <cstdio>
+
+// int main()
+// {
+//     printf("Hello World!");
+//     return 0;
+// }
